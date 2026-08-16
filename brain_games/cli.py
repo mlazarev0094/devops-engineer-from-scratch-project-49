0adfb75e311ac140eb0b46cf91175c7be53d17ed
Maksim Lazarev <mlazarev0094@gmail.com>
2026-08-16 14:27:03 +0300

    add output display

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -6,3 +6,16 @@ def welcome_user():
     name = prompt.string('May I have your name? ')
     print(f"Hello, {name}!")
     return name
+
+
+def get_user_unswer():
+    return input("Your answer: ").strip().lower()
+
+
+def display_correct():
+    print("Correct!")
+
+
+def display_wrong(user_answer, correct_answer, name):
+    print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+    print(f"Let's try again, {name}!")
