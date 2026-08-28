f955bda44cf035aef17a255b84b7c65f99fc13ee
Maksim Lazarev <mlazarev0094@gmail.com>
2026-08-28 21:03:06 +0300

    add game brain_progression

---
+++ brain_games/games/progression.py
@@ -0,0 +1,20 @@
+import random
+
+
+def progression_generate(start, step):
+    return [start + index * step for index in range(10)]
+
+
+def generate_question_and_answer():
+    start = random.randint(1, 100)
+    step  = random.randint(1, 100)
+    progression = progression_generate(start, step)
+    hidden_index = random.randint(0, 9)
+    correct_answer = str(progression[hidden_index])
+    progression[hidden_index] = ".."
+    question = "Question: " + " ".join(map(str, progression))
+    return correct_answer, question
+
+
+def get_rules():
+    return 'What number is missing in the progression?'
---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,10 @@
+from brain_games.engine import run_game
+from brain_games.games.progression import generate_question_and_answer, get_rules
+
+
+def main():
+    run_game(generate_question_and_answer, get_rules)
+
+
+if __name__ == '__main__':
+    main()
